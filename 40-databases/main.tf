990755a8fb99f1d1aae95a19a50c82dd0a1d9af2
SaiThota <thotasaiprasad180@gmail.com>
2026-03-09 10:29:58 +0530

    tf

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -119,3 +119,43 @@ resource "terraform_data" "mysql" {
   }
 }
 #=======================================================================================
+
+resource "aws_instance" "mysql" {
+  ami                     = local.ami_id
+  instance_type           = var.instance_type
+  subnet_id               = local.database_snet[0]
+  vpc_security_group_ids  = [local.mysql_sg_id]
+  iam_instance_profile    = aws_iam_instance_profile.mysql_profile.name
+  tags = merge(
+    local.common_tags,
+ {
+    Name =  "${var.project}-${var.environment}-mysql"
+ },
+    var.ec2_tags
+  )
+}
+
+resource "terraform_data" "mysql" {   
+  triggers_replace = [
+    aws_instance.redis.id,
+  ]
+
+  connection {   
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mysql.private_ip  
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh" 
+    destination = "/tmp/bootstrap.sh"    
+  }
+
+  provisioner "remote-exec" {   #after connecting it executes this values
+     inline = [
+        "chmod +x /tmp/bootstrap.sh",   #giving execution permission
+        "sudo sh /tmp/bootstrap.sh mysql dev"     #running that bootstarp.sh
+    ]
+  }
+}
