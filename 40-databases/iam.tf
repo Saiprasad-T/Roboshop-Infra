7af6497732f59bc83aee79da0ca1cd5950913936
SaiThota <thotasaiprasad180@gmail.com>
2026-03-09 11:32:04 +0530

    tf

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -61,7 +61,7 @@ resource "aws_iam_policy" "rabbitmq" {
 }
 
 #attaching policy to the role
-resource "aws_iam_role_policy_attachment" "mysql_policy" {
+resource "aws_iam_role_policy_attachment" "rabbitmq_policy" {
   role       = aws_iam_role.rabbitmq_role.name
   policy_arn = aws_iam_policy.rabbitmq.arn
 }
