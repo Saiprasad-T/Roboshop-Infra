ab033b8e8adcb800dd018b83d3e0cd5b0b72e06a
SaiThota <thotasaiprasad180@gmail.com>
2026-03-11 13:49:54 +0530

    alb

--- 50-alb/locals.tf
+++ 50-alb/locals.tf
@@ -1,5 +1,5 @@
 locals {
-    private_snet = split(",", data.aws_ssm_parameter.private-snet.value)[0]
+    private_snet = split(",", data.aws_ssm_parameter.private-snet.value)
     backend_alb-sg-id = data.aws_ssm_parameter.backend_alb-sg-id.value
 }
 
