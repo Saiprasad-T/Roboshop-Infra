9fd7be33caa6321b28cfd3615990a304bb7a66d3
SaiThota <thotasaiprasad180@gmail.com>
2026-03-21 08:30:30 +0530

    new

--- 10-sg/parameter.tf
+++ 10-sg/parameter.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "sg_id" {
   for_each = var.sg_names
-  name  = "/${var.project}/${var.environment}/${each.key}-sg-id" #roboshop/dev/catalogue-sg_id
+  name  = "/${var.project}/${var.environment}/${replace(each.key, "_", "-")}-sg-id" #roboshop/dev/catalogue-sg_id
   type  = "String"
   value = module.sg[each.key].sg_id
 }
