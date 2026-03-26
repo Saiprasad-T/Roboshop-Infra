b2cc08009b7ee61f027848bff0e4aa71ea26642a
SaiThota <thotasaiprasad180@gmail.com>
2026-03-26 16:00:51 +0530

    new

--- 50-alb/parameter.tf
+++ 50-alb/parameter.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "listner_arn" {
-  name  = "/${var.project}/${var.environment}/listner_arn" 
+resource "aws_ssm_parameter" "listener_arn" {
+  name  = "/${var.project}/${var.environment}/listener_arn" 
   type  = "String"
   value = aws_lb_listener.default_listner.arn
 }
