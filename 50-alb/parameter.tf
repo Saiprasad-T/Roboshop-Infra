8d95c446c8a616e6e4715a350895085d4604da6c
SaiThota <thotasaiprasad180@gmail.com>
2026-03-29 09:51:49 +0530

    new

--- 50-alb/parameter.tf
+++ 50-alb/parameter.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "listener_arn" {
-  name  = "/${var.project}/${var.environment}/backend-listener_arn" 
+resource "aws_ssm_parameter" "backend-listener_arn" {
+  name  = "/${var.project}/${var.environment}/backend_listener_arn" 
   type  = "String"
   value = aws_lb_listener.default_listener.arn
    overwrite = true
