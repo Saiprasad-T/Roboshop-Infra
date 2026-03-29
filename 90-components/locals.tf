50a7cd169a75567bfb756b2d9fe1d56a427bcbd9
SaiThota <thotasaiprasad180@gmail.com>
2026-03-29 09:00:48 +0530

    new

--- 90-components/locals.tf
+++ 90-components/locals.tf
@@ -2,9 +2,9 @@ locals {
   components = {
     catalogue = { priority = 10 }
     user      = { priority = 20 }
-    cart      = { priority = 30 }
+  /*   cart      = { priority = 30 }
     shipping  = { priority = 40 }
-    payment   = { priority = 50 }
-    frontend  = { priority = 10}
+    payment   = { priority = 50 } */
+    frontend  = { priority = 10} 
   }
 }
